55121011c0a74371c61bc17d437339dbf70cb42e
shahmeer <theshahmeerfsd@gmail.com>
2025-03-14 16:01:24 +0500

    Implement input handling

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,15 @@ function App() {
     phone: "",
   });
 
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    setFromData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <>
       <div>
@@ -15,15 +24,33 @@ function App() {
         <form action="">
           <div>
             <label htmlFor="username">Username: </label>
-            <input type="text" id="username" name="username" />
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="email">Email: </label>
-            <input type="text" id="email" name="email" />
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
           </div>
           <div>
             <label htmlFor="phone">Phone: </label>
-            <input type="tel" id="phone" name="phone" />
+            <input
+              type="tel"
+              id="phone"
+              name="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
           </div>
         </form>
       </div>
